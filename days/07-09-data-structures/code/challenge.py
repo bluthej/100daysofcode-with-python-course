915b8535281c8743bb93896c1fc3d32c11d0c225
bluthej <joffrey.bluthe@e.email>
2023-10-04 07:25:49 +0200

    Add challenge

---
+++ days/07-09-data-structures/code/challenge.py
@@ -0,0 +1,6 @@
+from data import us_state_abbrev, states_list
+
+print(f"10th item for us_state_abbrev: {list(us_state_abbrev.items())[9]}")
+print(f"10th item for states_list: {states_list[9]}")
+print(f"45th key for us_state_abbrev: {list(us_state_abbrev)[44]}")
+print(f"27th value for us_state_abbrev: {list(us_state_abbrev.values())[26]}")
